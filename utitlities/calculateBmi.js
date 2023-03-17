76c37f02a11b5516e29c7f1db4897dc05f33da2a
kolawole olaniyi <kolaniyi3@gmail.com>
2023-03-17 16:42:27 +0100

    calculate bmi feature completed

--- utitlities/calculateBmi.js
+++ utitlities/calculateBmi.js
@@ -1,6 +1,6 @@
 const calculateBmi = (weight, height) => {
   const bmi = weight / ((height / 100) * (height / 100));
-  return bmi.toFixed(1);
+  return bmi.toFixed(2);
 };
 
 export default calculateBmi;
